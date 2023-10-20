42f07ae0f005103920ca98aec13cf0471efb1fd2
QuarterColon <ankitchakraborty1972@gmail.com>
2023-10-20 08:18:00 +0530

    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import './App.css';
 
 
-import Routers from './routers/Routers';
 
-function App() {
-  return (
-    <div>
-      <Routers />
-=======
-=======
-
->>>>>>> 0ad88eff108aa237b98b724b5064d204a313e46c
 import "./App.css";
 import Login from "./components/Auth/login";
 import Register from "./components/Auth/register";
@@ -23,16 +11,16 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/landing" element={<Landing/>} />
         <Route path="/register" element={<Register />} />
       </Routes>
-<<<<<<< HEAD
->>>>>>> ankit
-=======
 
 
->>>>>>> 0ad88eff108aa237b98b724b5064d204a313e46c
+
+
+
+
     </div>
   );
 }
